fe73535479330f8eba04c62541b321714d098043
Drew Short <nerian.drew@gmail.com>
2015-03-23 15:18:59 -0500

    Cleaning up

--- src/str_util.rs
+++ src/str_util.rs
@@ -1,13 +1,13 @@
 /**
-    Converts a vector of types that support to_string  into a string
+    Converts a vector of types that have the ToString trait into 
+    a pretty string
 */
 pub fn string_for_vector<T: ToString>(vec: Vec<T>) -> String {
     let mut s = String::new();
-    for i in 0..vec.len() {
-        s.push_str(&vec[i].to_string());
-        if i + 1 < vec.len() {
-            s.push(',');
-        }
+    for x in &vec {
+        s.push_str(&x.to_string());
+        s.push(',');
     }
-    return s;
+    s.pop();
+    s
 }
