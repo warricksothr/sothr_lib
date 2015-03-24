e3922c660586c1755043e2ba171366f2d35d64a8
Drew Short <nerian.drew@gmail.com>
2015-03-24 10:44:36 -0500

    Changed doc to block style comments

--- src/str_util.rs
+++ src/str_util.rs
@@ -1,18 +1,18 @@
-///
-/// Converts a vector of types that have the ToString trait into 
-/// a pretty string
-///
-/// # Arguments
-///
-/// * 'vec' - The vector you'd like a pretty string for
-///
-/// # Examples
-///
-/// ```rust
-/// let v = vec![1,2,3]
-/// string_for_vector(v) // prints "1,2,3"
-/// ```
-///
+/**
+ Converts a vector of types that have the ToString trait into 
+ a pretty string
+
+ # Arguments
+
+ * 'vec' - The vector you'd like a pretty string for
+
+ # Examples
+
+ ```rust
+ let v = vec![1,2,3]
+ string_for_vector(v) // prints "1,2,3"
+ ```
+*/
 pub fn string_for_vector<T: ToString>(vec: Vec<T>) -> String {
     let mut s = String::new();
     for x in &vec {
